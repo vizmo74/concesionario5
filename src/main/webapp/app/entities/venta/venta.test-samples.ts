bcb27d8d1315fecb7be0d9708313db4c7c11a997
vizmo74 <vizmo74@gmail.com>
2022-09-21 13:58:06 +0200

    detalles de la entidad venta

--- src/main/webapp/app/entities/venta/venta.test-samples.ts
+++ src/main/webapp/app/entities/venta/venta.test-samples.ts
@@ -6,24 +6,29 @@ export const sampleWithRequiredData: IVenta = {
   id: 20448,
   total: 11440,
   fecha: dayjs('2022-09-18'),
+  vehiculo:
+
 };
 
 export const sampleWithPartialData: IVenta = {
   id: 32725,
   total: 11474,
   fecha: dayjs('2022-09-19'),
-};
+  vehiculo:
+  };
 
 export const sampleWithFullData: IVenta = {
   id: 56225,
   total: 71903,
   fecha: dayjs('2022-09-18'),
+  vehiculo:
 };
 
 export const sampleWithNewData: NewVenta = {
   total: 41728,
   fecha: dayjs('2022-09-19'),
   id: null,
+  vehiculo:
 };
 
 Object.freeze(sampleWithNewData);
